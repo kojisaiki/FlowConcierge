85405893d40142628d604d0f6eaa88a22543e57e
kojisaiki <saikou9901@gmail.com>
2019-05-06 14:15:28 +0900

    update gulp to v4 and run build success
    dev (watch) is not successed

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,19 @@
 import requireDir from 'require-dir'
 
-// Check out the tasks directory
-// if you want to modify tasks!
-requireDir('./tasks')
+requireDir('./tasks/lib')
+
+require('./tasks/chromereload')
+require('./tasks/clean')
+require('./tasks/fonts')
+require('./tasks/images')
+require('./tasks/locales')
+require('./tasks/manifest')
+require('./tasks/pages')
+require('./tasks/scripts')
+require('./tasks/styles')
+require('./tasks/version')
+
+require('./tasks/build')
+
+require('./tasks/pack')
+require('./tasks/default')
