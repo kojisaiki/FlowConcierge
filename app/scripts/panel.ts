f453fa990051c5eab40dd066ce0c70a5d55d9deb
kojisaiki <saikou9901@gmail.com>
2019-05-11 13:08:31 +0900

    recognize own page update

--- app/scripts/panel.ts
+++ app/scripts/panel.ts
@@ -1,3 +1,5 @@
+const OWN_TAB_ID = chrome.devtools.inspectedWindow.tabId;
+
 let env_id = document.getElementById('environment-id');
 if (env_id) {
   env_id.innerHTML = 'env!';
@@ -17,22 +19,25 @@ let status_element = document.getElementById('status');
 
 const port = chrome.runtime.connect({ name: 'devtools' });
 port.onMessage.addListener(
-  (msg: {
+  (payload: {
     msg: string;
     tabId: number;
     changeInfo: chrome.tabs.TabChangeInfo;
     tab: chrome.tabs.Tab;
   }) => {
+    if (payload.tabId !== OWN_TAB_ID) return;
+
+    if (!status_element) return;
+
     // Write information to the panel, if exists.
     // If we don't have a panel reference (yet), queue the data.
-    if (!status_element) return;
-    if (msg.msg === 'FlowEditor') {
-      status_element.innerHTML = 'FlowEditor!: ' + JSON.stringify(msg);
+    if (payload.msg === 'FlowEditor') {
+      status_element.innerHTML = 'FlowEditor!: ' + JSON.stringify(payload);
     } else {
-      status_element.innerHTML = 'any msg: ' + JSON.stringify(msg);
+      status_element.innerHTML = 'any msg: ' + JSON.stringify(payload);
     }
 
-    chrome.tabs.query({ index: msg.tab.index }, tabs => {
+    chrome.tabs.query({ index: payload.tab.index }, tabs => {
       if (hostname) {
         hostname.innerHTML = JSON.stringify(tabs[0].url);
       }
