868561be4c965352762c3aa5dffbb7d7758a35e4
kojisaiki <saikou9901@gmail.com>
2019-05-13 23:14:10 +0900

    tips panel move to more from

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,8 +1,12 @@
 import { tips } from './tips';
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  let isFlowEditor = false;
+let setup: boolean = false;
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (setup) return;
+  setup = true;
+
+  let isFlowEditor = false;
   if (document.querySelectorAll('div.manage-flow-section').length === 1) {
     isFlowEditor = true;
 
